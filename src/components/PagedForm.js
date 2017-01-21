8dee673e3b0cc0b2ef39317c4ed12de4ccab9d0e
Konrad Galuszka <konrad.galuszka@ocado.com>
2017-01-22 00:00:33 +0100

    Add interactive paging

--- src/components/PagedForm.js
+++ src/components/PagedForm.js
@@ -1,12 +1,96 @@
 import React from 'react';
+import {Grid, Row, Col, Image, PageHeader, Radio, ProgressBar, Pagination, small, ButtonToolbar, Button} from 'react-bootstrap';
+import {questions} from '../data/questions';
 
 export default class PagedForm extends React.Component {
-    render() {
-        return (
-            <div class="container">
-                <span>Paged form here! TODO </span>
-            </div>
+    constructor() {
+        super();
+        this.state = {
+            currentQuestionIndex: 1
+        };
+    }
 
+    render() {
+        let currentQuestion = () => {
+            return questions[this.state.currentQuestionIndex];
+        };
+
+        let changeToNextQuestion = () => {
+            this.setState({currentQuestionIndex: (this.state.currentQuestionIndex + 1) % questions.length})
+        };
+
+        let changeToPrevQuestion = () => {
+            let nextIndex = this.state.currentQuestionIndex - 1;
+            nextIndex = nextIndex >= questions.length ? 0 : nextIndex;
+            nextIndex = nextIndex < 0 ? questions.length - 1 : nextIndex;
+            this.setState({currentQuestionIndex: nextIndex})
+        };
+
+        let changeToQuestion = (index) => {
+            this.setState({currentQuestionIndex: index - 1});
+        };
+
+        let getProgress = () => {
+          return Math.floor(((this.state.currentQuestionIndex + 1) / questions.length ) * 100);
+        };
+
+        return (
+            <Grid>
+                <Row className="show-grid">
+                    <Col xs={6} md={8}>
+                        <PageHeader>Another brick in the wall <small>Pink Floyd</small></PageHeader>
+                    </Col>
+                </Row>
+                <Row className="show-grid">
+                    <Col xs={6} md={5}>
+                        <Image src={"/images/" + currentQuestion().img} responsive/>
+                    </Col>
+                    <Col xs={6} md={7}>
+                        <h3>{currentQuestion().label}</h3>
+                        <strong>1. Pytanie pierwsze, Pytanie pierwsze, Pytanie pierwsze, Pytanie pierwsze</strong>
+                        <Radio checked readOnly>
+                            Tak
+                        </Radio>
+                        <Radio readOnly>
+                            Nie
+                        </Radio>
+                        <strong>2. Pytanie drugie, Pytanie drugie, Pytanie drugie, Pytanie drugie</strong>
+                        <Radio readOnly>
+                            Tak
+                        </Radio>
+                        <Radio checked readOnly>
+                            Nie
+                        </Radio>
+                        <strong>3. Pytanie trzecie, Pytanie trzecie, Pytanie trzecie, Pytanie trzecie,</strong>
+                        <Radio checked readOnly>
+                            Tak
+                        </Radio>
+                        <Radio readOnly>
+                            Nie
+                        </Radio>
+                        <ButtonToolbar>
+                            <Button bsSize="large" onClick={changeToPrevQuestion} active>Wstecz</Button>
+                            <Button bsStyle="primary" bsSize="large" onClick={changeToNextQuestion} active>Następne pytanie</Button>
+                        </ButtonToolbar>
+                    </Col>
+                </Row>
+                <hr/>
+                <Row className="show-grid text-center">
+                    <ProgressBar now={getProgress()} label={`${getProgress()}%`} />
+                        <Pagination
+                            prev
+                            next
+                            first
+                            last
+                            ellipsis
+                            boundaryLinks
+                            items={questions.length}
+                            maxButtons={10}
+                            activePage={this.state.currentQuestionIndex + 1}
+                            onSelect={changeToQuestion} />
+                </Row>
+                <hr/>
+            </Grid>
         );
     }
-}
+};
---
+++ src/data/questions.js
@@ -0,0 +1,5 @@
+export const questions = [
+    {label: "Klasyczny rock", img: "pink-floyd.png"},
+    {label: "Jazz", img: "miles-davis.png"},
+    {label: "Pop", img: "michael-jackson.jpg"}
+];
