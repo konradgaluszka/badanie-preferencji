cce982dc104566d0cd1d194c6d63c73c6936db4d
Konrad Galuszka <konrad.galuszka@ocado.com>
2017-02-22 00:18:51 +0100

    Add hello page

--- src/components/PagedForm.js
+++ src/components/PagedForm.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Grid, Row, Col, Image, PageHeader, Radio, ProgressBar, Pagination, small, ButtonToolbar, Button} from 'react-bootstrap';
 import {questions, songs} from '../data/questions';
 import MusicLoop from './MusicLoop';
+import HelloPage from './HelloPage';
 import AnswerForSingleQuestion from './AnswerForSingleQuestion';
 
 export default class PagedForm extends React.Component {
     constructor() {
         super();
         this.state = {
+            locationInForm: 'hello',
             currentSongIndex: 1,
             answers: songs.map(() => {return []})
         };
@@ -27,7 +29,12 @@ export default class PagedForm extends React.Component {
         };
 
         let changeToPrevSong = () => {
-            this.setState({currentSongIndex: Math.max(this.state.currentSongIndex - 1, 0)})
+            if (this.state.currentSongIndex == 0) {
+                this.setState({locationInForm: 'hello'})    ;
+            } else {
+                this.setState({currentSongIndex: this.state.currentSongIndex - 1});
+            }
+
         };
 
         let changeToSong = (index) => {
@@ -55,6 +62,60 @@ export default class PagedForm extends React.Component {
         let wellStyle = {
           background: '#fefcfc'
         };
+
+        let getPageContent = () => {
+          if (this.state.locationInForm === 'hello') {
+              return (
+                  <Row className="show-grid" >
+                      <HelloPage changeToNextPage={() => {
+                          this.state.locationInForm = 'listen';
+                          this.setState(this.state);
+                      }}/>
+                  </Row>
+              );
+          } else if (this.state.locationInForm === 'listen') {
+              return (
+                  <Row className="show-grid well" style={wellStyle}>
+                      <Col xs={6} md={5} className="text-center">
+                          <MusicLoop song={currentSong().file}/>
+                      </Col>
+                      <Col xs={6} md={7}>
+                          <AnswerForSingleQuestion
+                              questionId="firstQuestion"
+                              question={"1. " + questions[0].text}
+                              isYesChecked={answerForQuestion(0)(true)}
+                              isNoChecked={answerForQuestion(0)(false)}
+                              answerChanged={answerChangedForQuestion(0)}
+                          />
+                          <AnswerForSingleQuestion
+                              questionId="secondQuestion"
+                              question={"2. " + questions[1].text}
+                              isYesChecked={answerForQuestion(1)(true)}
+                              isNoChecked={answerForQuestion(1)(false)}
+                              answerChanged={answerChangedForQuestion(1)}
+                          />
+                          <AnswerForSingleQuestion
+                              questionId="thirdQuestion"
+                              question={"3. " + questions[2].text}
+                              isYesChecked={answerForQuestion(2)(true)}
+                              isNoChecked={answerForQuestion(2)(false)}
+                              answerChanged={answerChangedForQuestion(2)}
+                          />
+
+                          <ButtonToolbar>
+                              <Button bsSize="large" onClick={changeToPrevSong}>
+                                  Wstecz</Button>
+                              <Button bsStyle="primary" bsSize="large" onClick={changeToNextSong}
+                                      disabled={this.state.currentSongIndex == songs.length - 1}>Następne pytanie</Button>
+                          </ButtonToolbar>
+                      </Col>
+                  </Row>
+              );
+          } else {
+              throw new Error("Unhandled location in form");
+          }
+        };
+
         return (
             <Grid>
                 <Row className="show-grid">
@@ -62,44 +123,10 @@ export default class PagedForm extends React.Component {
                         <PageHeader>Część słuchowa <small>Posłuchaj i odpowiedz na pytania</small></PageHeader>
                     </Col>
                 </Row>
-                <Row className="show-grid well" style={wellStyle}>
-                    <Col xs={6} md={5} className="text-center">
-                        <MusicLoop song={currentSong().file}/>
-                    </Col>
-                    <Col xs={6} md={7}>
-                        <AnswerForSingleQuestion
-                            questionId="firstQuestion"
-                            question={"1. " + questions[0].text}
-                            isYesChecked={answerForQuestion(0)(true)}
-                            isNoChecked={answerForQuestion(0)(false)}
-                            answerChanged={answerChangedForQuestion(0)}
-                        />
-                        <AnswerForSingleQuestion
-                            questionId="secondQuestion"
-                            question={"2. " + questions[1].text}
-                            isYesChecked={answerForQuestion(1)(true)}
-                            isNoChecked={answerForQuestion(1)(false)}
-                            answerChanged={answerChangedForQuestion(1)}
-                        />
-                        <AnswerForSingleQuestion
-                            questionId="thirdQuestion"
-                            question={"3. " + questions[2].text}
-                            isYesChecked={answerForQuestion(2)(true)}
-                            isNoChecked={answerForQuestion(2)(false)}
-                            answerChanged={answerChangedForQuestion(2)}
-                        />
-
-                        <ButtonToolbar>
-                            <Button bsSize="large" onClick={changeToPrevSong} disabled={this.state.currentSongIndex == 0}>
-                                Wstecz</Button>
-                            <Button bsStyle="primary" bsSize="large" onClick={changeToNextSong}
-                                    disabled={this.state.currentSongIndex == songs.length - 1}>Następne pytanie</Button>
-                        </ButtonToolbar>
-                    </Col>
-                </Row>
+                {getPageContent()}
                 <hr/>
                 <Row className="show-grid text-center">
-                    <ProgressBar now={getProgress()} label={`${getProgress()}%`} />
+                    <ProgressBar now={getProgress()} label={`${getProgress()}%`}/>
                         <Pagination
                             prev
                             next
