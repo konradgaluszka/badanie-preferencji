e6ea68233c4dae0b6ad13bf74abaa46debd12e97
Konrad Galuszka <konrad.galuszka@ocado.com>
2017-02-22 22:48:57 +0100

    Extract answers to separate component

--- src/components/HelloPage.js
+++ src/components/HelloPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Jumbotron, Button} from 'react-bootstrap';
+import {Row, Jumbotron, Button} from 'react-bootstrap';
 
 
 export default class HelloPage extends React.Component {
@@ -9,13 +9,18 @@ export default class HelloPage extends React.Component {
 
     render() {
         return (
-            <Jumbotron>
-                <h1>Hello, world!</h1>
-                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
-                    This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
-                    This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-                <p><Button bsStyle="primary" onClick={this.props.changeToNextPage}>Rozpocznij słuchanie</Button></p>
-            </Jumbotron>
+            <Row className="show-grid">
+                <Jumbotron>
+                    <h1>Hello, world!</h1>
+                    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+                        featured content or information.
+                        This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+                        featured content or information.
+                        This is a simple hero unit, a simple jumbotron-style component for calling extra attention to
+                        featured content or information.</p>
+                    <p><Button bsStyle="primary" onClick={this.props.changeToNextPage}>Rozpocznij słuchanie</Button></p>
+                </Jumbotron>
+            </Row>
         );
     }
 };
--- src/components/PagedForm.js
+++ src/components/PagedForm.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import {Grid, Row, Col, Image, PageHeader, Radio, ProgressBar, Pagination, small, ButtonToolbar, Button} from 'react-bootstrap';
 import {questions, songs} from '../data/questions';
-import MusicLoop from './MusicLoop';
 import HelloPage from './HelloPage';
-import AnswerForSingleQuestion from './AnswerForSingleQuestion';
+import MusicFormPages from './MusicFormPages';
 
 export default class PagedForm extends React.Component {
     constructor() {
         super();
         this.state = {
             locationInForm: 'hello',
-            currentSongIndex: 1,
-            answers: songs.map(() => {return []})
+            currentSongIndex: -1
         };
     }
 
     render() {
-        let boolean = (b) => {
-            return b === 'true';
-        };
-
-        let currentSong = () => {
-            return songs[this.state.currentSongIndex];
+        let changeToSong = (index) => {
+            this.setState({locationInForm: 'listen', currentSongIndex: index - 1});
         };
 
         let changeToNextSong = () => {
@@ -30,89 +24,55 @@ export default class PagedForm extends React.Component {
 
         let changeToPrevSong = () => {
             if (this.state.currentSongIndex == 0) {
-                this.setState({locationInForm: 'hello'})    ;
+                //TODO: support saving answers on component unmount
+                //this.setState({locationInForm: 'hello', currentSongIndex: -1});
             } else {
                 this.setState({currentSongIndex: this.state.currentSongIndex - 1});
             }
-
-        };
-
-        let changeToSong = (index) => {
-            this.setState({currentSongIndex: index - 1});
         };
 
         let getProgress = () => {
           return Math.floor(((this.state.currentSongIndex + 1) / songs.length ) * 100);
         };
 
-        let answerChangedForQuestion = (questionIndex) => {
-            return (inputValue) => {
-                let stateChange = this.state.answers;
-                stateChange[this.state.currentSongIndex][questionIndex] = boolean(inputValue.currentTarget.value);
-                this.setState(stateChange);
-            };
-        };
-
-        let answerForQuestion = (questionIndex) => {
-            return (answer) => {
-                return this.state.answers[this.state.currentSongIndex][questionIndex] === answer;
-            };
-        };
-
-        let wellStyle = {
-          background: '#fefcfc'
-        };
-
         let getPageContent = () => {
           if (this.state.locationInForm === 'hello') {
               return (
-                  <Row className="show-grid" >
-                      <HelloPage changeToNextPage={() => {
-                          this.state.locationInForm = 'listen';
-                          this.setState(this.state);
-                      }}/>
-                  </Row>
+                      <HelloPage changeToNextPage={() => changeToSong(1)}/>
               );
           } else if (this.state.locationInForm === 'listen') {
               return (
-                  <Row className="show-grid well" style={wellStyle}>
-                      <Col xs={6} md={5} className="text-center">
-                          <MusicLoop song={currentSong().file}/>
-                      </Col>
-                      <Col xs={6} md={7}>
-                          <AnswerForSingleQuestion
-                              questionId="firstQuestion"
-                              question={"1. " + questions[0].text}
-                              isYesChecked={answerForQuestion(0)(true)}
-                              isNoChecked={answerForQuestion(0)(false)}
-                              answerChanged={answerChangedForQuestion(0)}
-                          />
-                          <AnswerForSingleQuestion
-                              questionId="secondQuestion"
-                              question={"2. " + questions[1].text}
-                              isYesChecked={answerForQuestion(1)(true)}
-                              isNoChecked={answerForQuestion(1)(false)}
-                              answerChanged={answerChangedForQuestion(1)}
-                          />
-                          <AnswerForSingleQuestion
-                              questionId="thirdQuestion"
-                              question={"3. " + questions[2].text}
-                              isYesChecked={answerForQuestion(2)(true)}
-                              isNoChecked={answerForQuestion(2)(false)}
-                              answerChanged={answerChangedForQuestion(2)}
-                          />
-
-                          <ButtonToolbar>
-                              <Button bsSize="large" onClick={changeToPrevSong}>
-                                  Wstecz</Button>
-                              <Button bsStyle="primary" bsSize="large" onClick={changeToNextSong}
-                                      disabled={this.state.currentSongIndex == songs.length - 1}>Następne pytanie</Button>
-                          </ButtonToolbar>
-                      </Col>
-                  </Row>
+                  <MusicFormPages questions={questions}
+                                  songs={songs}
+                                  currentSongIndex={() => this.state.currentSongIndex}
+                                  changeToNextSong={changeToNextSong}
+                                  changeToPrevSong={changeToPrevSong}
+                  />
               );
           } else {
               throw new Error("Unhandled location in form");
+          }
+        };
+
+        let getPagination = () => {
+          if (this.state.locationInForm === 'listen') {
+              return (
+                  <Pagination
+                      prev
+                      next
+                      first
+                      last
+                      ellipsis
+                      boundaryLinks
+                      items={songs.length}
+                      maxButtons={10}
+                      activePage={this.state.currentSongIndex + 1}
+                      onSelect={changeToSong} />
+              );
+          }  else {
+              return (
+                  <div></div>
+              );
           }
         };
 
@@ -127,17 +87,7 @@ export default class PagedForm extends React.Component {
                 <hr/>
                 <Row className="show-grid text-center">
                     <ProgressBar now={getProgress()} label={`${getProgress()}%`}/>
-                        <Pagination
-                            prev
-                            next
-                            first
-                            last
-                            ellipsis
-                            boundaryLinks
-                            items={songs.length}
-                            maxButtons={10}
-                            activePage={this.state.currentSongIndex + 1}
-                            onSelect={changeToSong} />
+                    {getPagination()}
                 </Row>
                 <hr/>
             </Grid>
